7f1a1a9939d70c864f522c051ab83c4680becc12
aaryaman shinde <aaryamanshinde@aaryamans-Mac-mini.local>
2025-02-17 13:18:57 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Spline from "@splinetool/react-spline";
+import Game from "./Game";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Main Page */}
+        <Route
+          path="/"
+          element={
+            <div className="App">
+              <Spline scene="https://prod.spline.design/lEcfxn6gjSRxgaZd/scene.splinecode" />
+              <Link to="/game" className="valentine-button">💝 Play Valentine's Game 💝</Link>
+            </div>
+          }
+        />
+        {/* Game Page */}
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </Router>
   );
 }
 
